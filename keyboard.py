fcdab45b95cdbf170f6b90921ae237ec6624f8c8
Herrick Fang <herrickfang@gmail.com>
2017-02-04 02:09:38 +0000

    connected motor

--- keyboard.py
+++ keyboard.py
@@ -15,6 +15,7 @@ MATRIX = [ [1, 2, 3, 'A'],
 
 ROW = [7 ,11 ,13 ,15]
 COL = [12, 16, 18, 22]
+MOTORS = [33, 35, 37]
 
 for j in range(4):
     GPIO.setup(COL[j], GPIO.OUT)
@@ -23,23 +24,70 @@ for j in range(4):
 for i in range(4):
     GPIO.setup(ROW[i], GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
+
+for k in range(3):
+    GPIO.setup(MOTORS[k], GPIO.OUT)
+    
 state = 0
+storage = ""
+#old = time.time()
+
+GPIO.output(MOTORS[2], GPIO.LOW)
+    
+def unlock():
+    print(" OPEN MOTORS HIGH ")
+    GPIO.output(MOTORS[0], GPIO.HIGH)
+    GPIO.output(MOTORS[1], GPIO.LOW)
+    GPIO.output(MOTORS[2], GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(MOTORS[2], GPIO.LOW)
+        
+
+
+def wrong():
+    print("wrong")
+    GPIO.setup(40, GPIO.OUT)
+    GPIO.output(40, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(40, GPIO.LOW)
+    time.sleep(0.05)
+    GPIO.output(40, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(40, GPIO.LOW)
+    time.sleep(0.05)
+    GPIO.output(40, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(40, GPIO.LOW)
 
 try:
     while(True):
-        now = time.time()
+        
         for j in range(4):
             GPIO.output(COL[j], 0)
             for i in range(4):
                 if GPIO.input(ROW[i]) == 0:
+                    storage += str(MATRIX[i][j])
+                    print("code: {} state: {}".format(MATRIX[i][j], state))
+                    print(storage)
+
+                    if state == 3:
+                        if storage == "1234":
+                            unlock()
+                        else:
+                            wrong()
                     state += 1
-                    next_now = time.time()
-                    if next_now - now > 2:
-                        state =0
-                    print("code: {} state: {}".format(MATRIX[i][j]), state)
+                    if state >= 4:
+                        storage = ""   
+                        state = 0
+                    #now = time.time()
+                    # if now- old > 10 and state = 0:
+                      #  state = 0
+                      #  old = now
+                    
                     while(GPIO.input(ROW[i])== 0):
                         pass
 
+            time.sleep(0.02)
             GPIO.output(COL[j], 1)
 except KeyboardInterrupt:
     GPIO.cleanup()
